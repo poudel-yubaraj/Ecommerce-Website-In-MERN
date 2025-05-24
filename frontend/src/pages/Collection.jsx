ed4ac55dcc2b9d618d565c009a34636cf9306c87
= <poudel.yubaraj42@gmail.com>
2025-05-24 17:20:58 +0545

    Adding a sorting based on price range

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -2,54 +2,71 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import { assets } from "../assets/assets";
 import Title from "../components/Title";
-import ProductItem from '../components/ProductItem'
+import ProductItem from "../components/ProductItem";
 const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
-  const[filterProducts, setFilterProducts]= useState([]);
-  const[category, setCategory]= useState([]);
-  const[subCategory,setSubCategory]= useState([]);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState("relevant");
 
-   const toggleCategory = (e)=>{
+  const toggleCategory = (e) => {
     console.log(e.target.value);
-      if(category.includes(e.target.value)){
-          setCategory(prev=>prev.filter(item => item!==e.target.value))
-       }else{
-        setCategory(prev=>[...prev,e.target.value])
-       }
-   }
-
-   const toggleSubCategory = (e)=>{
-    if(subCategory.includes(e.target.value)){
-    setSubCategory(prev=>prev.filter(item=>item!=e.target.value))
-    }else{
-      setSubCategory(prev=>[...prev,e.target.value])
+    if (category.includes(e.target.value)) {
+      setCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setCategory((prev) => [...prev, e.target.value]);
     }
-   }
-//for applying  category and subcategory filter
-   const applyFilter = ()=>{
+  };
+
+  const toggleSubCategory = (e) => {
+    if (subCategory.includes(e.target.value)) {
+      setSubCategory((prev) => prev.filter((item) => item != e.target.value));
+    } else {
+      setSubCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  //for applying  category and subcategory filter
+  const applyFilter = () => {
     let productsCopy = products.slice();
-    if(category.length>0){
-      productsCopy=productsCopy.filter(item=>category.includes(item.category));
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        category.includes(item.category)
+      );
     }
-    if(subCategory.length){
-      productsCopy=productsCopy.filter(item=>subCategory.includes(item.subCategory))
+    if (subCategory.length>0) {
+      productsCopy = productsCopy.filter((item) =>
+        subCategory.includes(item.subCategory)
+      );
     }
+  debugger;
+    // Apply sorting
+    if (sortType === "low-high") {
+      debugger;
+      console.log("from low to high");
+      productsCopy.sort((a, b) => a.price - b.price);
+    } else if (sortType === "high-low") {
+      productsCopy.sort((a, b) => b.price - a.price);
+    }
+
     setFilterProducts(productsCopy);
-   }
+  };
 
-  // useEffect(()=>{
-  //   setFilterProducts(products);
-  // },[])
+  //for initally displaying the products
+  useEffect(() => {
+    setFilterProducts(products);
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     applyFilter();
-  },[category,subCategory])
-  
+  }, [category, subCategory,sortType]);
+
   //for checking purposes
-  useEffect(()=>{
-      console.log(subCategory)
-  },[subCategory])
+  useEffect(() => {
+    console.log(subCategory);
+  }, [subCategory]);
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -151,20 +168,30 @@ const Collection = () => {
           {/* Title */}
           <Title text1={"ALL"} text2={"COLLECTION"} />
           {/* Sort Dropdown */}
-          <select className="border border-gray-300 text-sm sm:text-base px-3 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-pink-500 transition duration-200">
+          <select
+            onChange={(e) => setSortType(e.target.value)}
+            className="border border-gray-300 text-sm sm:text-base px-3 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-pink-500 transition duration-200"
+          >
             <option value="relevant">Sort by: Relevant</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
           </select>
         </div>
-      
 
-{/* mapping the item */}
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-          {filterProducts.map((item,index)=>{
-            return <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
+        {/* mapping the item */}
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+          {filterProducts.map((item, index) => {
+            return (
+              <ProductItem
+                key={index}
+                name={item.name}
+                id={item._id}
+                price={item.price}
+                image={item.image}
+              />
+            );
           })}
-      </div>
+        </div>
       </div>
     </div>
   );
